b3c140bd6d9e150d0b314cb8b07da652f798bfd7
Juho Soivio <soivio.juho@gmail.com>
2022-11-22 10:22:43 +0200

    viimeinen commit issueen #3

--- frontend/src/components/register.js
+++ frontend/src/components/register.js
@@ -1,61 +1,74 @@
-import React, { Component } from "react";
-import "../App.css"
+import React, {useState} from 'react'
+import "./register.css"
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import iceland from './images/iceland-g207af6228_1920'
 
+export default function Register() {
+    
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
 
-class Register extends Component {
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log(e.target.username.value);
-    console.log(e.target.password.value)
-    console.log(e.target.password2.value);
+  function credentialsAsRequested() {
 
-    if (!e.target.username.value) {
-      alert("Username is required");
-    } else if (!e.target.username.value) {
-      alert("Valid Username is required");
-    } else if (!e.target.password.value) {
-      alert("Password is required");
-    }else if (e.target.password2.value!==e.target.password.value){
-      alert("Passwords dont match")
-      } else if (
-      e.target.username.value === "asdasdasdasd" &&
-      e.target.password.value === "123456" &&
-      e.target.password2.value === "123456"
-    ) {
-      alert("Successfully logged in");
-      e.target.username.value = "";
-      e.target.password.value = "";
-      e.target.password2.value = "";
-    } 
-      
-  };
+      setUsername(document.getElementById("usernameInput").value)
+      setPassword(document.getElementById("passwordInput").value)
 
+      const formData = new FormData();
+      formData.append('username', username)
+      formData.append('password', password)
 
-
-  render() {
-    return (
-      <div className="Register">
-      
-        <form className="form" onSubmit={this.handleSubmit}>
-          <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username"/>
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password2">Password</label>
-            <input id="password2" type="password" name="password2" />
-          </div>
-          <button className="primary">Register</button>
-        </form>
-        <Link className="link" to='/'>Back to Login</Link>
-      </div>
-    );
+      axios.post('http://localhost:8080/register', formData)
+          .then(response => localStorage.setItem("token", response.data))
+          .catch(e => console.log(e.message))
   }
-}
 
-export default Register;
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(e.target.username.value);
+        console.log(e.target.password.value)
+        console.log(e.target.password2.value);
+    
+        if (!e.target.username.value) {
+          alert("Username is required")
+        } else if (!e.target.username.value) {
+          alert("Valid Username is required")
+        } else if (!e.target.password.value) {
+          alert("Password is required")
+        }else if (e.target.password2.value!==e.target.password.value){
+          alert("Passwords dont match")
+          } else if (
+          e.target.password.value === e.target.password2.value
+        ) {
+          credentialsAsRequested()
+          alert("Successfully registered user")
+          e.target.username.value = ""
+          e.target.password.value = ""
+          e.target.password2.value = ""
+        } 
+      };
+
+  return (
+      <div className="Register">
+          <form className="form" onSubmit={handleSubmit}>
+              <div className="input-group">
+                  <label htmlFor="username">Username</label>
+                  <input id='usernameInput' type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+              </div>
+              <div className="input-group">
+                  <label htmlFor="password">Password</label>
+                  <input id='passwordInput' type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+              </div>
+              <div className="input-group">
+                  <label htmlFor="password2">Password</label>
+                  <input id="password2" type="password" name="password2"/>
+              </div>
+              <div className='Registerandlogin'>
+              <button className="primary">Register</button>
+              <Link className="link" to='/'>Back to Login</Link>
+              <img className='ideland' src=''></img>
+              </div>
+          </form>
+      </div>
+  )
+}
