524d2ec601dae7accb83b775aec2c41567e67596
Jussi <t1leju02@students.oamk.fi>
2022-11-28 10:07:48 +0200

    muutoksia JussinLogin.js tiedostoon

--- frontend/src/components/JussinLogin.js
+++ frontend/src/components/JussinLogin.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
-import axios from 'axios';
-import {Buffer} from 'buffer';
+import axios, { HttpStatusCode } from 'axios';
+import { Buffer } from 'buffer';
 
 export default function JussinLogin() {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [access, setAccess] = useState()
 
     function credentialsAsRequestParams() {
 
@@ -18,64 +19,66 @@ export default function JussinLogin() {
             .then(response => localStorage.setItem("token", response.data))
             .catch(e => console.log(e.message))
 
-        //console.log(axios.response.data)
+            // if (access === "200") {
+            //     console.log("Toimii")
+            // }else{
+            //     console.log("Ei toimi")
+            // }
     }
 
-    function requestWithBearerToken(){
+    function requestWithBearerToken() {
 
         //Bearer token from localstorage for the request
         const config = {
-          headers:{
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-          },
-          withCredentials: true
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            withCredentials: true
         }
-    
+
         axios.get('http://localhost:8080/private', config)
-          .then(response => console.log(response.data))
-          .catch( e => console.log(e.message))
-      }
-    
-      /**
-       * Sends credentials as base 64 coded basic authorization header
-       */
-      const credentialsAsBasicAuth = ()=>{
-    
+            .then(response => console.log(response.data))
+            .catch(e => console.log(e.message))
+    }
+
+    /**
+     * Sends credentials as base 64 coded basic authorization header
+     */
+    const credentialsAsBasicAuth = () => {
+
         //Base64 coding the string username:password
-        const base64credentials =  Buffer.from(`${username}:${password}`).toString('base64');
-       
+        const base64credentials = Buffer.from(`${username}:${password}`).toString('base64');
+
         const config = {
-          headers:{
-            'Authorization': `Basic ${base64credentials}`
-          },
-          withCredentials: true
+            headers: {
+                'Authorization': `Basic ${base64credentials}`
+            },
+            withCredentials: true
         }
-    
+
         axios.post('http://localhost:8080/private', {}, config)
-          .then(response => console.log(response.data))
-          .catch( e => console.log(e.message))
-      }
+            .then(response => console.log(response.data))
+            .catch(e => console.log(e.message))
+    }
 
     function login(e) {
         e.preventDefault();
         credentialsAsRequestParams();
-        requestWithBearerToken();
-        credentialsAsBasicAuth();
+        //requestWithBearerToken();
+        //credentialsAsBasicAuth();
     }
 
     return (
-        <>
-            <div>
-                <form>
-                    <div>
-                        <input id='usernameInput' value={username} onChange={e => setUsername(e.target.value)}/>
-                    </div>
-                    <div>
-                        <input id='passwordInput' value={password} onChange={e => setPassword(e.target.value)}/>
-                    </div>
-                    <button onClick={login}>k</button>
-                </form>
-            </div>
-        </>
+        <div>
+            <form>
+                <div>
+                    <input id='usernameInput' value={username} onChange={e => setUsername(e.target.value)} />
+                </div>
+                <div>
+                    <input id='passwordInput' value={password} onChange={e => setPassword(e.target.value)} />
+                </div>
+                <button onClick={login}>k</button>
+            </form>
+        </div>
     )
 }
