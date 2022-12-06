c73ffe2cae63dd6b10be503cd3cb722f99079c3e
TeeAr00 <t1rate00@student.oamk.fi>
2022-12-06 10:08:57 +0200

    ensimmäiseen graaffin tieto basesta

--- frontend/src/components/Info.js
+++ frontend/src/components/Info.js
@@ -1,18 +1,10 @@
 import React, { useState,useEffect } from 'react'
 import axios from 'axios'
+import { Line,Pie } from 'react-chartjs-2';
 
 export default function Info() {
 
-  // const [Data1, setData1]= useState ([]);
-  // useEffect(() => {
-  //   const getData1=async()=>{
-  //   const res = await fetch ('http://localhost:8080/Info');
-  //   const getdata= await res.json();
-  //   setData1(getdata);
-  //   console.log(getdata);
-  //   }
-  //   getA();
-  // },[])
+  
   const [infot, setInfot] = useState([])
   
     useEffect(()=>{
@@ -22,31 +14,97 @@ export default function Info() {
         console.log(results);
        
         console.log(results.data);
-        setInfot(results.data.infot);
+        setInfot(results.data);
       }
       getInfot();
       
     },[]);
-    //     axios
-    //     .get('http://localhost:8080/info')
-    //     .then(res =>{
-    //         console.log(res)
-    //         setData1(res.data)
-    //     })
-    //     .catch(err =>{
-    //         console.log(err)
-    //     })
-    // })
+    
+    const data = {
+        labels: infot.map(y=> y.year),
+                
+        datasets: [
+          {
+            label: "Anomaly",
+            data: infot.map(y=> y.anomaly),
+            borderColor:  "rgb(1, 99, 132)",
+            backgroundColor: "rgba(1, 99, 132, 0.5)",
+            yAxisID: "temp",
+            parsing: {
+              xAxisKey: "Time",
+              yAxisKey: "temp",
+            },
+          }, 
+          {
+            label: "Lower",
+            data: infot.map(y=> y.lower_confidence_limit),
+            borderColor:  "rgb(255, 99, 132)",
+            backgroundColor: "rgba(255, 99, 132, 0.5)",
+            yAxisID: "temp",
+            parsing: {
+              xAxisKey: "Time",
+              yAxisKey: "temp",
+            },
+          },
+          {
+            label: "Upper",
+            data: infot.map(y=> y.upper_confidence_limit),
+            borderColor:  "rgb(53, 162, 235)",
+            backgroundColor: "rgba(53, 162, 235, 0.5)",
+            yAxisID: "temp",
+            parsing: {
+              xAxisKey: "Time",
+              yAxisKey: "temp",
+          },  
+            pointRadius: 1,
+          },    
+        ],
+      };
+    
+      const options = {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: "top",
+          },
+          title: {
+            display: true,
+            text: "Test temp1",
+          },
+        },
+        scales: {
+          temp: {
+            type: "linear",
+            display: true,
+            position: "right",
+          },      
+        },
+      };
+   
   
 
   return (
     <div>
-      moi 
-      {infot}
+       <div className="App">
+       <div style={{ width: "600px" }}></div>
+        <Line options={options} data={data} /> 
+        
+          </div>
+      {/* <ul>
+        {infot.map(info =>{
+          return <tr>
+            <td>
+              {info.year} /
+              {info.anomaly}/
+              {info.upper_confidence_limit}/
+              {info.lower_confidence_limit}
+            </td>
+          </tr>
+        })}
+      </ul> */}
+      
     </div>
-    // <div>
-    //   {infot}
-    // </div>
+    
 
   )
 }
